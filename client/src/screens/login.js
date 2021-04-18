298ab7d1c827c3faee59607e8e42e21d41b2fc2a
Muhammad Zain <muhammadzainulabdin123@gmail.com>
2021-04-18 15:44:18 +0500

    Login page updated with material UI

--- client/src/screens/login.js
+++ client/src/screens/login.js
@@ -1,37 +1,100 @@
-import React, { useState, useEffect } from 'react';
-import '../css/login.css';
-import Axios from 'axios'
+import React, { useState, useEffect, state, useRef } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export default function Login() {
-    const [name, setname]=useState('');
-    const [pass,setPass]=useState('');
 
-    const submitReview = () =>{
-        Axios.post('http://localhost:3001/api/login', {
-            name: name, 
-            pass: pass,
-        }).then(()=>{
-                alert("successful login");
-            });
-    };
-  return(
-    <div className="login-wrapper">
-      <h1>Log In</h1>
-      <form>
-        <label>
-          <p>Username</p>
-          <input type="text" 
-          onChange={text=>setname(text.target.value)}/>
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" 
-          onChange={text=>setPass(text.target.value)}/>
-        </label>
-        <div>
-          <button type="submit" onClick={submitReview}>Submit</button>
-        </div>
-      </form>
-    </div>
-  )
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+  const [name, setName]=useState('');
+  const [pass, setPass]=useState('');
+  const nameValue = useRef('');
+  const passValue = useRef('');
+  const sendValue = () => {
+    alert(passValue.current.value);
+}
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="User name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            inputRef={nameValue}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            inputRef={passValue}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={sendValue}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+      <Box mt={8}>
+      </Box>
+    </Container>
+  );
 }
